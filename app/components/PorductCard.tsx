5cc2c43fd0df3b8f530a97fab93c6911b478084b
Daniel Uirbe <danielu35@gmail.com>
2023-10-25 15:44:46 -0700

    Setting up tailwind and daisy UI css

---
+++ app/components/PorductCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddToCart from './AddToCart';
+
+const PorductCard = () => {
+  return (
+    <div>
+      <AddToCart />
+    </div>
+  )
+}
+
+export default PorductCard
