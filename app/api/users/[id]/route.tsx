c8982086c4142a168c3df7fdbaa1990053fb1997
Daniel Uirbe <danielu35@gmail.com>
2023-11-02 14:15:11 -0700

    Updating an Object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -16,3 +16,23 @@ export function GET(
 
   return NextResponse.json({ id: 1, name: "Daniel" });
 }
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  // Validate the request body
+  const body = await request.json();
+  if (!body.name)
+    // If the request body is invalid, return a 400 error
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+
+  // Fetch use with given id
+  if (params.id > 10)
+    // If user does not exist, return 400
+    return NextResponse.json({ error: "User not found" }, { status: 400 });
+
+  // Update user
+  // Return the updated user
+  return NextResponse.json({ id: 1, name: body.name });
+}
