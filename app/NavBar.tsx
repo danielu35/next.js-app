77489ce5f469ac0108cbfe2cdcd823187ba0fa95
Daniel Uirbe <danielu35@gmail.com>
2023-11-09 00:48:31 -0700

    accessing session on the client

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,16 +1,40 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
 
 const NavBar = () => {
-  return (
-    <div className='flex bg-slate-200 p-5 space-x-3'>
-      <Link href={'/'} className='mr-5'>Next.Js</Link>
-      <Link href={'/users'} className='mr-5'>Users</Link>
-      <Link href={'/admin'} className='mr-5'>Admin</Link>
-      <Link href={'/products'} className='mr-5'>Products</Link>
-      <Link href={'/api/auth/signin'}>Login</Link>
-    </div>
-  )
-}
+  const { status, data: session } = useSession();
 
-export default NavBar
+  return (
+    <div className="flex bg-slate-200 p-5 space-x-3">
+      <Link href={"/"} className="mr-5">
+        Next.Js
+      </Link>
+      <Link href={"/users"} className="mr-5">
+        Users
+      </Link>
+      <Link href={"/admin"} className="mr-5">
+        Admin
+      </Link>
+      <Link href={"/products"} className="mr-5">
+        Products
+      </Link>
+      {status === "loading" && (
+        <span className="loading loading-spinner loading-sm absolute right-10" />
+      )}
+      {status === "authenticated" && (
+        <div className="absolute right-10">{`Signed in as: ${
+          session.user!.name
+        }`}</div>
+      )}
+      {status === "unauthenticated" && (
+        <Link href={"/api/auth/signin"} className="absolute right-10">
+          Login
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default NavBar;
