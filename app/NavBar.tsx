49597c5166773f5b9f06dab2cbac8399cc312830
Daniel Uirbe <danielu35@gmail.com>
2023-11-09 00:08:02 -0700

    adding authentication

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,11 +3,12 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div className='flex bg-slate-200 p-5'>
+    <div className='flex bg-slate-200 p-5 space-x-3'>
       <Link href={'/'} className='mr-5'>Next.Js</Link>
       <Link href={'/users'} className='mr-5'>Users</Link>
       <Link href={'/admin'} className='mr-5'>Admin</Link>
-      <Link href={'/products'}>Products</Link>
+      <Link href={'/products'} className='mr-5'>Products</Link>
+      <Link href={'/api/auth/signin'}>Login</Link>
     </div>
   )
 }
---
+++ app/upload/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+import React, { useState } from "react";
+import { CldImage, CldUploadWidget } from "next-cloudinary";
+
+interface CloudinaryResult {
+  public_id: string;
+}
+
+const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
+  return (
+    <>
+      {publicId && (
+        <CldImage alt={"Rio"} src={publicId} width={300} height={500} />
+      )}
+      <CldUploadWidget
+        options={{
+          sources: ["local"],
+          multiple: true,
+          maxFiles: 5,
+          styles: {
+            palette: {
+              window: "#3FB14C",
+              windowBorder: "#90A0B3",
+              tabIcon: "#0078FF",
+              menuIcons: "#5A616A",
+              textDark: "#000000",
+              textLight: "#FFFFFF",
+              link: "#0078FF",
+              action: "#FF620C",
+              inactiveTabIcon: "#0E2F5A",
+              error: "#F44235",
+              inProgress: "#0078FF",
+              complete: "#20B832",
+              sourceBg: "#E4EBF1",
+            },
+            fonts: {
+              default: {
+                active: true,
+              },
+            },
+          },
+        }}
+        uploadPreset="xpvu8ehd"
+        onUpload={(result, widget) => {
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
+  );
+};
+
+export default UploadPage;
